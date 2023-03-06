99cf9fd4e26a1d256432ffb8cf00759ea41244eb
olianderson <olianderson1@gmail.com>
2023-03-06 11:01:53 -0300

    :sparkles: feat: [200~

---
+++ src/main/java/com/repositorio/estudospring/notificacao/NotificadorEmail.java
@@ -0,0 +1,17 @@
+package com.repositorio.estudospring.notificacao;
+
+import org.springframework.stereotype.Component;
+
+import com.repositorio.estudospring.model.Cliente;
+
+@Component  
+public class NotificadorEmail {
+
+    public NotificadorEmail() {
+        System.out.println("Construtor NotificadorEmail chamado");
+    }
+
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificação %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+}
